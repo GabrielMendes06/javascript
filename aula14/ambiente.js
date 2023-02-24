85cceec447ce110fb58ca5405f4089fd3d9c5390
Gabriel Mendes da Silva <122908813+GabrielMendes06@users.noreply.github.com>
2023-02-24 17:04:54 -0300

    Create ambiente.js

---
+++ aula14/ambiente.js
@@ -0,0 +1,14 @@
+/*console.log('Vai começar...')
+for(var c = 1; c <= 10;c++){
+    console.log(c)
+}
+console.log('FIM!')*/
+
+const data = [
+    { id: 1, comment: 'lol?' },
+    { id: 2, comment: 'lol?' }
+  ];
+  
+  for (let i = 0; i < data.length; i++) {
+    console.log(data[i].comment);
+  }
