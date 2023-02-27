3663e0a21ff549f1428fe57c424e08264df4eae4
Gabriel Mendes da Silva <122908813+GabrielMendes06@users.noreply.github.com>
2023-02-27 20:28:23 -0300

    atz

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -1,24 +1,24 @@
-function carregar(){
-    var msg = window.document.querySelector('#msg')
-    var img = window.document.querySelector('img#imagem')
-    var data = new Date()
-    //var hora = data.getHours()
-    var hora = 19
-    msg.innerHTML = `Agora são ${hora} horas`
-    if (hora >= 0 && hora < 6) {
-        //Bom dia
-        img.src = 'imagens/madrugada.jpg'
-        document.body.style.background = '#3C150E'
-    } else if (hora >= 6 && hora < 12) {
-        img.src = 'imagens/manha.jpg'
-        document.body.style.background = '#e2cd9f'
-    } else if (hora >= 12 && hora < 18) {
-        //Boa tarde
-        img.src = 'imagens/tarde.jpg'
-        document.body.style.background = '#b9846f'
-    } else {
-        //Boa noite
-        img.src = 'imagens/noite.jpg'
-        document.body.style.background = "#4A484F"
-    }
+function carregar() {
+  var msg = window.document.querySelector("#msg");
+  var img = window.document.querySelector("img#imagem");
+  var data = new Date();
+  //var hora = data.getHours()
+  var hora = 19;
+  msg.innerHTML = `Agora são ${hora} horas`;
+  if (hora >= 0 && hora < 6) {
+    //Bom dia
+    img.src = "imagens/madrugada.jpg";
+    document.body.style.background = "#3C150E";
+  } else if (hora >= 6 && hora < 12) {
+    img.src = "imagens/manha.jpg";
+    document.body.style.background = "#e2cd9f";
+  } else if (hora >= 12 && hora < 18) {
+    //Boa tarde
+    img.src = "imagens/tarde.jpg";
+    document.body.style.background = "#b9846f";
+  } else {
+    //Boa noite
+    img.src = "imagens/noite.jpg";
+    document.body.style.background = "#4A484F";
+  }
 }
--- aula14 ex/Modelo/script.js
+++ aula14 ex/Modelo/script.js
@@ -1,31 +1,34 @@
 function contar() {
-    let ini = document.getElementById('txti') 
-    let fim = document.getElementById('txtf')
-    let passo = document.getElementById('txtp')
-    let res = document.getElementById('res')
+  let ini = document.getElementById("txti");
+  let fim = document.getElementById("txtf");
+  let passo = document.getElementById("txtp");
+  let res = document.getElementById("res");
 
-    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
-        res.innetHTML = 'Impossível contar!'
-        window.alert('[ERRO] faltam dados!')
-    } else {
-        res.innerHTML = 'Contando: <br> '
-        let i = Number(ini.value)
-        let f = Number(fim.value)
-        let p = Number(passo.value)
-        if (p <=0 ) {
-            window.alert('Passo invalido, considerando passo 1')
-            p = 1
-        }
-        if(i < f) {
-            for(let c = i ; c <= f ; c += p ) {
-            res.innerHTML += ` ${c} \u{1f449}`
-        }
-        
-        } else {
-            for(let c = i; c >= f; c -= p)  {
-                res.innerHTML += `${c} \u{1f449}`
-            }
-        }
-        res.innerHTML += '\u{1f3c1}'
+  if (
+    ini.value.length == 0 ||
+    fim.value.length == 0 ||
+    passo.value.length == 0
+  ) {
+    res.innetHTML = "Impossível contar!";
+    window.alert("[ERRO] faltam dados!");
+  } else {
+    res.innerHTML = "Contando: <br> ";
+    let i = Number(ini.value);
+    let f = Number(fim.value);
+    let p = Number(passo.value);
+    if (p <= 0) {
+      window.alert("Passo invalido, considerando passo 1");
+      p = 1;
     }
+    if (i < f) {
+      for (let c = i; c <= f; c += p) {
+        res.innerHTML += ` ${c} \u{1f449}`;
+      }
+    } else {
+      for (let c = i; c >= f; c -= p) {
+        res.innerHTML += `${c} \u{1f449}`;
+      }
+    }
+    res.innerHTML += "\u{1f3c1}";
+  }
 }
