a72f69b080ea3662125d8364348aac0b36f2fd91
Gabriel Mendes da Silva <122908813+GabrielMendes06@users.noreply.github.com>
2023-02-27 20:59:34 -0300

    a

--- aula14 ex/ex017/script.js
+++ aula14 ex/ex017/script.js
@@ -2,18 +2,15 @@ function calcular() {
   let num = document.querySelector("#num");
   let res = document.querySelector("#tabuada");
   res.innerText = ''  
-  for (let contador = 1; contador <= 10; contador++) {
+  for (let contador = 1; contador <= 10; contador++) { 
     if (num.value.length == 0) {
       alert("Digite um número, por favor");
       return;
-    }
-    if (num.value == 0) {
-      alert("[ERRO] tabuada começando do número 1");
-      num.value++;
     }
     let resposta = num.value * contador;
     let multiplicacao = document.createElement("p");
     multiplicacao.innerText = `${num.value} x ${contador} = ${resposta}`;
     res.appendChild(multiplicacao);
+    
   }
 }
