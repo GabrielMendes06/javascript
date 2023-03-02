58f3593ed8fa615084878d54e96ba7c6746a0295
Gabriel Mendes da Silva <122908813+GabrielMendes06@users.noreply.github.com>
2023-03-02 10:41:08 -0300

    Update script.js

--- aula14 ex/ex016/script.js
+++ aula14 ex/ex016/script.js
@@ -1,14 +1,2 @@
 let lista = [];
 
-function listar() {
-  let caixa = document.querySelector("#caixa");
-  let num = document.querySelector("#numero");
-  let valor = num.value;
-  if (valor < 1 || valor > 100) {
-    alert("ERRO, por favor, digite um número entre 1 e 100");
-  } else {
-    lista.push(Number(valor));
-    create;
-    document.querySelector("#numero").value = null;
-  }
-}
